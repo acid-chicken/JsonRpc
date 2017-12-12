b7ac0b1be6061ec7d1708ad7258d611bc0e6a2db
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2017-12-13 02:42:05 +0900

    Remove unused files

--- Publish.cmd
+++
@@ -1,8 +0,0 @@
-SET configuration=Release
-SET out=C:/Publish
-
-call dotnet pack ./src/EdjCase.JsonRpc.Core -c %configuration% -o "%out%/EdjCase.JsonRpc.Core"
-
-call dotnet pack ./src/EdjCase.JsonRpc.Router -c %configuration% -o "%out%/EdjCase.JsonRpc.Router"
-
-call dotnet pack ./src/EdjCase.JsonRpc.Client -c %configuration% -o "%out%/EdjCase.JsonRpc.Client"
